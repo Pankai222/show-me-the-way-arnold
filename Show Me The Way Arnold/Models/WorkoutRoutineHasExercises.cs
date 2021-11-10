3cbd6ddbfe940abd813db630d1ce27560fd2ac18
Olligagman <mikaelpasovski@gmail.com>
2021-11-10 22:22:59 +0100

    Added snackbar

--- Show Me The Way Arnold/Models/WorkoutRoutineHasExercises.cs
+++ Show Me The Way Arnold/Models/WorkoutRoutineHasExercises.cs
@@ -1,10 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace Show_Me_The_Way_Arnold.Models
 {
     public class WorkoutRoutineHasExercises
     {
         public int? Sets { get; set; }
         public int? Repetitions { get; set; }
-        public int ExerciseIdExercise { get; set; }
+        [JsonPropertyName("exerciseidexercise")]
+        public int ExerciseId { get; set; }
         public Exercise Exercise { get; set; }
     }
 }
