b9de6604c34998ae2e091d67f973edb02fae5dce
Olligagman <mikaelpasovski@gmail.com>
2021-11-10 21:45:27 +0100

    Implemented creation of workout with exercise

--- Show Me The Way Arnold/Models/WorkoutRoutineHasExercises.cs
+++ Show Me The Way Arnold/Models/WorkoutRoutineHasExercises.cs
@@ -2,8 +2,9 @@ namespace Show_Me_The_Way_Arnold.Models
 {
     public class WorkoutRoutineHasExercises
     {
-        public int Sets { get; set; }
-        public int Repetitions { get; set; }
+        public int? Sets { get; set; }
+        public int? Repetitions { get; set; }
+        public int ExerciseIdExercise { get; set; }
         public Exercise Exercise { get; set; }
     }
 }
