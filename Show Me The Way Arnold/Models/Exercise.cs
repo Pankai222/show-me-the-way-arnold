550a8be43e3aa02f992c20340add35bbdd6c125e
Olligagman <mikaelpasovski@gmail.com>
2021-11-16 21:47:48 +0100

    Refactoring of frontend to remove unnecessary variables and modelclasses

--- Show Me The Way Arnold/Models/Exercise.cs
+++ Show Me The Way Arnold/Models/Exercise.cs
@@ -1,13 +1,28 @@
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace Show_Me_The_Way_Arnold.Models
 {
     public class Exercise
     {
-        [JsonPropertyName("idexercise")]
-        public int Id { get; set; }
+        [JsonPropertyName("idexercise")] public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
         public bool Compound { get; set; }
+
+        public List<ExerciseHasMuscles> Muscles { get; set; }
+        public List<ExerciseHasWorkoutEquipment> Equipment { get; set; }
+    }
+
+    public class ExerciseHasMuscles
+    {
+        public int MuscleId { get; set; }
+        public Muscle Muscle { get; set; }
+    }
+
+    public class ExerciseHasWorkoutEquipment
+    {
+        public int WorkoutEquipmentId { get; set; }
+        public WorkoutEquipment WorkoutEquipment { get; set; }
     }
 }
--- Show Me The Way Arnold/Models/ExerciseHasMuscles.cs
+++
@@ -1,8 +0,0 @@
-namespace Show_Me_The_Way_Arnold.Models
-{
-    public class ExerciseHasMuscles
-    {
-        public int MuscleId { get; set; }
-        public Muscle Muscle { get; set; }
-    }
-}
